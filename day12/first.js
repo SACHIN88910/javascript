4dfc89af984c2430a4a60a8b2ad3cc1bc5ac3dfc
SACHIN88190 <sachinchoudhary88190@gmail.com>
2025-11-04 11:12:10 +0530

    day12

---
+++ day12/first.js
@@ -0,0 +1,77 @@
+//function
+// function structure
+// function greet(){
+//     console.log("hello kesa ho");
+// }
+
+// greet();
+
+//add program
+// function sum(num1,num2){
+//     console.log(num1+num2)
+// };
+// //passing arrument
+// sum(3,5);
+// sum(2,6);
+
+//multiplication
+//function multipliy(num1,num3){
+   // console.log(num1*num3);
+  // return num1*num3;
+//};
+
+//console.log(multipliy(7,5));
+
+// const fun1= function(){
+//     console.log("hello coder arvy");
+// }
+// fun1();
+// console.log(fun1);
+
+//arrow function
+
+// const fun= ()=>{
+//     console.log("hello coder");
+// }
+// fun();
+
+// const sum=(num1,num2)=>{
+//     return num1+num2;
+// }
+// console.log(sum(4,6));
+//advantage
+//make shortcut
+// const sum=(num1,num2)=>num1+num2;
+// console.log(sum(3,4));
+
+//  
+
+// const cube=number=> number*number*number;
+//  console.log(cube(9));
+
+// const fun=function(...number){
+    //for loop and sum nikal sakta hu
+//     console.log(number);
+
+// }
+
+// fun(4,5,6,1,43);
+
+// let obj={
+//     name:"rohir",
+//     age:30,
+//     amount:78
+// }
+
+// function fun(obj1){
+//     console.log(obj1.name,obj1.amount);
+// }
+// fun(obj);
+
+
+
+
+ 
+
+
+ 

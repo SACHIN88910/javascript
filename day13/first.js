e43d14764e1753e87e4c487b993ad05d791d4cef
SACHIN88190 <sachinchoudhary88190@gmail.com>
2025-11-05 11:13:00 +0530

    day13

---
+++ day13/first.js
@@ -0,0 +1,109 @@
+//if-else
+
+// let age=30;
+// if(age>=18)
+// {
+//     console.log("eligible for vote");
+
+// }
+// else
+// {
+//     console.log("not eligible for vote");
+// }
+//if else if else
+// let age=19;
+// if(age<18)
+// {
+//     console.log("kid");
+// }
+// else if(age>45)
+// {
+//     console.log("old");
+// }
+// else
+// {
+//     console.log("young");
+// }
+
+//multiple case switch
+
+// switch(new Date().getDay())
+// {
+//     case 0:
+//         console.log("sunday");
+//         break;
+
+//         case 1:
+//         console.log("mon");
+//         break;
+//         case 2:
+//         console.log("tues");
+//         break;
+//         case 3:
+//         console.log("wed");
+//         break;
+//         case 5:
+//         console.log("thus");
+//         break;
+//         case 6:
+//         console.log("fri");
+//         break;
+//         case 6:
+//         console.log("fri");
+//         break;
+//         default:
+//             console.log("not a valid day");
+
+
+// }
+
+//loop
+
+// for(let i=0;i<20;i++)
+// {
+//     console.log("hello coder army");
+// }
+
+//sum first n number
+// let sum=0;
+// for(let i=0;i<=10;i++)
+// {
+//     sum+=i;
+// }
+// console.log(sum);
+
+//nested for loop
+// for(let i=0;i<5;i++)
+// {
+//     for(let j=0;j<=i;j++)
+//     {
+//         console.log("*");
+        
+//     }
+// }
+
+
+
+
+
+//   let arr=[10,3,0,6,5];
+//   for(let i=0;i<arr.length;i++)
+//   {
+//     console.log(arr[i]);
+//   }
+
+
+// const obj={
+//     name:"rohit",
+//     age:30,
+//     amount:420,
+//     city:"delhi"
+// }
+// const key=Object.keys(obj);
+// for(let i=0;i<key.length;i++)
+// {
+//         console.log(obj[key[i]]);
+// }
+
+
+//for in loop

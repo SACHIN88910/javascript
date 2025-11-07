7f3c3f6b4eccba54b637e60ccbb35130dc537e5b
SACHIN88190 <sachinchoudhary88190@gmail.com>
2025-11-07 22:46:48 +0530

    day15

---
+++ day15/first.js
@@ -0,0 +1,109 @@
+//for of loop
+
+// const arr=[10,20,11,18,13];
+// for(let value of arr)
+// {
+//     console.log(value);
+// }
+
+// let str =" sachon is the good boy";
+
+// for(let vlaue of str)
+// {
+//     console.log(vlaue);
+// }
+
+// donit use on loop in object
+//symble dot itteratiable not define for  object
+
+// const obj ={
+//     name:"chavi",
+//     age:34,
+//     gender:"female",
+// }
+//  for(let value of obj)
+//  {
+//     console.log(value);
+//  }
+
+//    for(let value of Object.keys(obj) )
+//    {
+//     console.log(value,obj[value]);
+//    }
+
+  //for each
+  // let arr=[13,64,75,35];
+  // arr.forEach(function(num){
+  //       console.log(num);
+
+  // })
+
+    // arr.forEach((num)=>{
+    //             console.log(num);
+        
+    //       })
+
+    // arr.forEach(function greet(num){
+    //   console.log(num);
+    
+    //  })
+
+    //
+    // single argument:number
+    //second index
+    // third:array ko bhi pass
+    //arr.forEach(call back function)
+  //   arr.forEach((num,index,a)=>{console.log(num,index)
+  //       a[index]=num*2;
+  //   }
+  // );
+  //  console.log(arr);
+
+  // function greet(num){
+  //   console.log(num);
+  // }
+  // arr.forEach(greet);
+
+   //filter
+   
+  //  let arr1=[22,41,33,50,89];
+  //  const result=arr1.filter((num)=>{
+  //   return num%2==0;
+    //return true or false
+  //  })
+  //  console.log(result);
+
+  
+  // const stutend=[
+  //   {name:"rohan",age:20,marks:53},
+  //   {name:"harsh",age:22,marks:34},
+  //   {name:"shubham",age:22,marks:13},
+  //   {name:"dikshant",age:22,marks:70},
+  //   {name:"sachin",age:22,marks:70},
+    
+  // ];
+  // const result=stutend.filter((obj)=>{
+  //      return obj.marks>50;
+  // })
+  // console.log(result);
+    
+  // const result=stutend.filter((obj)=> obj.marks>50);
+  // console.log(result);
+  // const result=stutend.filter(({marks})=> marks>50);
+  // console.log(result);
+    
+  //map
+//   let arr2=[1,2,4,5,6];
+//   const result=arr2.map((num)=>{
+//     return num*num;
+//  })
+//  console.log(result);
+// const arr4=[1,2,3,4,5,6];
+//  const result =arr4.filter((num)=>num%2==0).map((num)=>num*num);
+//  console.log(result);
+  
+//reduce function
+
+
+
+
---
+++ day15/index.js
@@ -0,0 +1,28 @@
+// call back funsion
+//  function names(fun){
+//     console.log("i am sachin");
+//     fun();
+//  }
+
+//   function greet(){
+//     console.log(" i am call back function");                                                     
+//   }
+//   names(greet);// this is called call back function   
+
+  
+// names(()=>{
+//     console.log(" i am call back function");
+// })
+
+// names(function greet(){
+//         console.log(" i am call back function");
+//       })
+
+//   function fetchData(){
+//     console.log(" i am fetching data");
+//   }
+//   setInterval(fetchData,5000);
+  
+
+
+  
